dc4eeb6b60a6f94be7240c2a9d2f83ee0b1aa4b0
mrdwright <mrdwright@shaw.ca>
2016-04-07 12:37:47 -0700

    Initial Commit
    adding my files

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import AwesomeComponent from './AwesomeComponent';
+
+export default class App extends React.Component {
+  render() {
+    return <AwesomeComponent />;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'dist');
+var APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'app.js'
+  },
+  module : {
+   loaders : [
+     {
+       test : /\.jsx?$/,
+       include : APP_DIR,
+       loader : 'babel',
+       exclude: /node_modules/
+     }
+   ]
+ }
+};
+
+module.exports = config;
